4683ea42259d26bb8d8f85d90dbf4c0658c550d5
abdullah12ka4 <itsjackabdullah25@gmail.com>
2025-06-16 14:20:07 +0500

    Uploaded

--- app/component/Footer.js
+++ app/component/Footer.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { FaGithub, FaInstagram, FaYoutube, FaLinkedin } from 'react-icons/fa';
 
 
 export default function Footer() {
@@ -11,16 +12,35 @@ export default function Footer() {
     ]
   return (
     <footer className="bg-gray-900 text-white py-3">
-      <div className="max-w-7xl mx-auto px-4 flex flex-col-reverse lg:flex-row-reverse justify-between items-center">
+      <div className="max-w-7xl mx-auto px-4 flex flex-col-reverse justify-between items-center gap-4 md:gap-10">
         <p className="text-sm mt-3 lg:mt-0 text-center">&copy; {new Date().getFullYear()} Designed and Created by Abdullah. All rights reserved.</p>
-        <div className='flex flex-col items-center gap-2'>
+        <div className='flex gap-4 flex-col md:flex-row justify-between w-[70vw]'>
+          <div className='flex flex-col items-center gap-2 mb-2'>
             <h1 className='text-xl font-semibold'>Categories</h1>
         <div className="flex space-x-4">
             {navLinks.map((links)=>(
                 <Link key={links.path} href={links.path} className="hover:underline capitalize">{links.name}</Link>
-            ))}
-          
-          
+            ))} 
+        </div>
+        </div>
+        <div className='flex flex-col items-center gap-2 mb-4'>
+            <h1 className='text-xl font-semibold'>Follow Me</h1>
+        <div className="flex space-x-4">
+             <div className="flex items-center gap-4 mt-1">
+            <Link href="https://github.com/abdullah12ka4" target="_blank">
+              <FaGithub className="text-2xl hover:text-yellow-400 transition" />
+            </Link>
+            <Link href="https://www.linkedin.com/in/abdullah-malik-769770309/" target="_blank">
+              <FaLinkedin className="text-2xl hover:text-yellow-400 transition" />
+            </Link>
+            <Link href="https://www.youtube.com/@itsabdullahtech" target="_blank">
+              <FaYoutube className="text-2xl hover:text-yellow-400 transition" />
+            </Link>
+            <Link href="https://www.instagram.com/_____abdullah_here/" target="_blank">
+              <FaInstagram className="text-2xl hover:text-yellow-400 transition" />
+            </Link>
+          </div>
+        </div>
         </div>
         </div>
       </div>
--- app/page.js
+++ app/page.js
@@ -74,7 +74,7 @@ export default function index() {
         </section>
         ))}</>}
 
-      <Footer />
+      <Footer/>
     </div>
   );
 }
